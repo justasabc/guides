5d5415afa8880c91b15e685a3521d505b0cb712f
zunlin1234 <zunlin1234@gmail.com>
2013-11-21 06:35:57 -0800

    finish openstack essex guide

---
+++ openstack/script/restart_nova.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+for a in libvirt-bin nova-network nova-compute nova-cert nova-api nova-objectstore nova-scheduler nova-volume novnc nova-consoleauth; do service "$a" stop; done
+
+for a in libvirt-bin nova-network nova-compute nova-cert nova-api nova-objectstore nova-scheduler nova-volume novnc nova-consoleauth; do service "$a" start; done
