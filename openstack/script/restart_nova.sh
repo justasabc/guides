ea3d620d2f2bcd8f0ec234f30c0fc8765a3b5006
zunlin1234 <zunlin1234@gmail.com>
2013-11-24 17:35:31 -0800

    reorganize linux guide structure and add system network disk virtualization guides

--- openstack/script/restart_nova.sh
+++ openstack/script/restart_nova.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
-for a in libvirt-bin nova-network nova-compute nova-cert nova-api nova-objectstore nova-scheduler nova-volume novnc nova-consoleauth; do service "$a" stop; done
+echo "killing dmidecode..."
+killall -9 dmidecode
+ps -ef | grep dmidecode
+echo "================================================================"
+virsh list
+echo "================================================================"
+for a in nova-network nova-compute nova-cert nova-api nova-objectstore nova-scheduler nova-volume novnc nova-consoleauth; do service "$a" stop; done
 
-for a in libvirt-bin nova-network nova-compute nova-cert nova-api nova-objectstore nova-scheduler nova-volume novnc nova-consoleauth; do service "$a" start; done
+for a in nova-network nova-compute nova-cert nova-api nova-objectstore nova-scheduler nova-volume novnc nova-consoleauth; do service "$a" start; done
+echo "================================================================"
+nova-manage service list
