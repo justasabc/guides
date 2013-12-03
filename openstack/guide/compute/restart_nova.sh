0544f9545fc594e4a6951254b7c0671d2f116454
zunlin1234 <zunlin1234@gmail.com>
2013-12-03 00:45:12 -0800

    remove openstack guides and scripts

--- openstack/guide/compute/restart_nova.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-for a in libvirt-bin nova-compute nova-api ; do service "$a" stop; done
-for a in libvirt-bin nova-compute nova-api ; do service "$a" start; done
-
--- openstack/guide/controller/restart_nova.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "===================================================="
-for a in libvirt-bin nova-network nova-compute nova-cert nova-api nova-objectstore nova-scheduler nova-volume novnc nova-consoleauth; do service "$a" stop; done
-
-for a in libvirt-bin nova-network nova-compute nova-cert nova-api nova-objectstore nova-scheduler nova-volume novnc nova-consoleauth; do service "$a" start; done
-echo "===================================================="
